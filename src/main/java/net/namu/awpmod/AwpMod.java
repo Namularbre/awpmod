9f796fc1f3fc2f897cfb5fc0892ba515d9448d3f
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-04-19 18:58:41 +0200

    added awp and awp ammo item

--- src/main/java/net/namu/awpmod/AwpMod.java
+++ src/main/java/net/namu/awpmod/AwpMod.java
@@ -1,10 +1,19 @@
 package net.namu.awpmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class AwpMod implements ModInitializer {
+    private static final Item AWP = new Item(new FabricItemSettings());
+    private static final Item AWP_AMMO = new Item(new FabricItemSettings());
+
     @Override
     public void onInitialize() {
-
+        Registry.register(Registries.ITEM, new Identifier("awpmod", "awp"), AWP);
+        Registry.register(Registries.ITEM, new Identifier("awpmod", "awp_ammo"), AWP_AMMO);
     }
 }
